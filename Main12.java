a162c7231f71e1989a66757d42e7ebb3a7ffcb65
Tsegab Gebregziabher <tg29285@email.vccs.edu>
2018-02-19 17:04:29 -0500

    initial

---
+++ Main12.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main12 {
+
+
+
+    public static void main(String[] args) {
+
+        Scanner keybord = new Scanner(System.in);
+
+        int sum =0;
+
+        int j = 6;
+
+        for(int i = 0; i < 10; i++) {
+
+            sum += i;
+
+
+
+            if(i < j) {
+
+                System.out.println(i);
+
+            } else {
+
+                System.out.println(j);
+
+                while(j<10) {
+
+                    j++;
+
+                } do {
+
+                    j++;
+
+                } while (j<10);
+
+                }
+
+            }
+
+            }
+
+
+
+    }
+
+
+
+
